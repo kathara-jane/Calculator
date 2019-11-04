292bf42cc3e04a7639f370b1c4cca0288fe99594
kathara-jane <katherine.j.sanborn@gmail.com>
2019-11-04 17:34:51 -0500

    Add files via upload

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,14 +1,10 @@
 TODO:
 /*
-1. numbers clicked appear on screen
-2. detect if button clicked is number or clear
-2. operands click take the number on the screen and DO the thing to the next number(s) clicked
-3. the doing doesn't take place until the user clicks equals, signaling that they have finished adding numbers in
-4. the equals sign demonstrates the sum or product of the numbers and the operand
-5. the C button hard refreshes and clears the calculator
-6. this means that number buttons signal buttons moving to screen, operand buttons signal 
-
-
+1. operands click take the number on the screen and DO the thing to the next number(s) clicked
+2 the doing doesn't take place until the user clicks equals, signaling that they have finished adding numbers in
+3. the equals sign demonstrates the sum or product of the numbers and the operand
+4. the C button hard refreshes and clears the calculator
+5. this means that number buttons signal buttons moving to screen, operand buttons signal 
 */
 
 
@@ -17,80 +13,62 @@ var start = "0";
 var pendingOperation = "";
 const nums = [];
 const screen = document.querySelector(".screen");
-const addButton = document.getElementById("addition")
-
+const addButton = document.getElementById("add")
+const subtractButton = document.getElementById("subtract")
+const divideButton = document.getElementById("divide")
+const multiplyButton = document.getElementById("multiply")
+const clearButton = document.getElementById("clear")
+const equalsButton = document.getElementById("equals")
 
 
 //this is where: setting up operations
 const operations = {
     add: (num1, num2) => add(num1,num2),
     subtract: (num1,num2) => subtract(num1,num2),
-    division: (num1,num2) => division(num1,num2),
+    divide: (num1,num2) => divide(num1,num2),
     multiply: (num1,num2) => multiply(num1,num2),
 }
 
-function add = (num1, num2) => {
+const add = (num1, num2) => {
     return num1 + num2;
 }
 
-function subtract = (num1, num2) => {
+const subtract = (num1, num2) => {
     return num1 - num2;
 }
 
-function division = (num1, num2) => {
+const divide = (num1, num2) => {
     return num1/ num2;
 }
 
-function multiply = (num1, num2) => {
+const multiply = (num1, num2) => {
     return num1 * num2;
-}
-
-
-
-
-
-console.log(count)
-/*const math = () =>{
-    if (typeOf === NaN) {
-        return 0
-    } else {
-        return number 
-    }
-    
-}*/
-
-const click = () => {
-    console.log("clicked")
-    if(isNaN(parseInt)) {
-        doSymbol;
-    } else {
-        doNumber;
-    }
-    rerender();
 }
 
 const onNumberButtonClick = e => {
     const num = e.target.innerText
-    screen.innerText = num
-    nums.push(num);
+        screen.innerText = num
+        nums.push(num);
 }
 
-const doSymbol = (value) => {
-    if(value === "C"){
-        count = 0
-        start = "0"
-    } else if (value === "÷"){
-
-    }
+const doMath = (nums, pendingOperation) => {
+    console.log(pendingOperation)
+    const result = operations[pendingOperation](parseInt(nums[0]),parseInt(nums[1]))
+    screen.innerText = result
 }
+
 
 function rerender() {
     screen.innerText = start;
   }
 
 const init = () => {
-    Array.from(document.getElementsByClassName('calc-button')).forEach(btn => btn.addEventListener('click', onNumberButtonClick))
-    addButton.addEventListener("click", () => pendingOperation= 'add')
+    Array.from(document.getElementsByClassName('number')).forEach(btn => btn.addEventListener('click', onNumberButtonClick))
+    addButton.addEventListener("click", () => pendingOperation= "add") 
+    subtractButton.addEventListener("click", () => pendingOperation= "subtract")
+    divideButton.addEventListener("click", () => pendingOperation= "divide")
+    multiplyButton.addEventListener("click", () => pendingOperation = "multiply")
+    equalsButton.addEventListener("click", () => doMath(nums, pendingOperation))
 }
 init();
 
@@ -104,3 +82,17 @@ init();
 //set nums []
 //set result to screen.innerText
 //else set screen inner to e.target.innerText and add to nums
+
+
+
+/*
+
+CLEAR BUTTON
+FIT ON SCREEN
+MULTIPLE NUMBERS, MULTIDIGIT NUMBERS
+ADD MORE OPERATIONS
+DECIMAL POINT
+
+overflow hidden
+
+*/
